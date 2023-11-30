3eb7e3e9c023d345373e294d1285da6ab23f0e82
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-11-30 10:23:52 +0000

    Empty Spring Project
    Added the empty files for the spring boot application (server).

---
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/Team7SohoKidsChristmasLightsApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.client1.team7sohokidschristmaslights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7SohoKidsChristmasLightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7SohoKidsChristmasLightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/cf/client1/team7sohokidschristmaslights/Team7SohoKidsChristmasLightsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.client1.team7sohokidschristmaslights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7SohoKidsChristmasLightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
