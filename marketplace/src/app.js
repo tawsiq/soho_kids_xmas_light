7dd6bebcdf175de56418f9340f32a5a91d60d862
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-03 23:21:04 +0000

    added a new function that dynamically generates products and adds them to the page after the data is fetched from the json. However there seems to be an issue

--- marketplace/src/app.js
+++ marketplace/src/app.js
@@ -1,10 +1,42 @@
 let iconCart = document.querySelector('.icon-cart');
 let closeCart =document.querySelector('.close')
 let body = document.querySelector('body')
+let listPrroductHTML = document.querySelector('.listProduct');
 
+let listProducts = [];
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
 closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
+
+
+const addDataToHTML = () => {
+    listPrroductHTML.innerHTML = '';
+    if(listProducts.length > 0){
+        listProducts.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.innerHTML = '' +
+                ` <img src="http://localhost:63342/untitled/marketplace/src/${product.image}" alt =" t-shirt design 1">\n' +
+                        <h2>${product.name}</h2>\n' +
+                        <div class="price">£${product.price}</div>\n' +
+                        <button class="addCart">\n' +
+                            Add To Cart\n' +
+                       </button>`;
+        })
+    }
+}
+const initApp = () => {
+    //get data from json
+    fetch('products.json')
+        .then(response => response.json())
+        .then(data => {
+            listProducts = data;
+            console.log(listProducts);
+            addDataToHTML();
+        })
+
+}
+initApp();
