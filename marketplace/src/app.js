1d7dede421cd9fd81d38d608010a8cee659c28d8
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-03 21:09:34 +0000

    added function to hide the checkout page when clicking the shopping cart icon (clicking it will make it appear, reclicking it will make it disappear), the close button should also hide the checkout page however it is not currently working.

---
+++ marketplace/src/app.js
@@ -0,0 +1,7 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeCart =document.querySelector('.close')
+let body = document.querySelector('body')
+
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart')
+})
