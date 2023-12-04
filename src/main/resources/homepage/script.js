5455fa103ea386e8e34bbf300a39091b915fa915
Mo <quddusm1@cardiff.ac.uk>
2023-12-04 15:39:28 +0000

    NavBar and Home Page layout

--- src/main/resources/homepage/script.js
+++ src/main/resources/homepage/script.js
@@ -1,46 +1,30 @@
-$(document).ready(function () {
-    // Fetch drawing submissions via AJAX
-    $.ajax({
-        url: 'getDrawings.php' , // Replace with your server-side script
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            if (data.length > 0) {
-                // Populate slides with drawing submissions
-                for (var i = 0; i < data.length; i++) {
-                    $('slideshow-container').append('<div class="mySlides">' +
-                      '<img src="' + data[i].imageURL + '" alt="Drawing ' + (i + 1) + '">' +
-                      '<div class="text">' + data[i].caption + '</div>' +
-                      '</div>');
-                }
+document.addEventListener('DOMContentLoaded', function () {
 
-                // Start the slideshow
-                showSlides(1);
-            } else {
-                console.error('No drawing submissions found.');
-            }
-        },
-        error: function (xhr, status, error) {
-            console.error('AJAX error:', status, error);
-        }
-    });
+    <!-- List of Image URLs -->
+
+    var imageUrls = [
+        '../static/homepage content/lights_pic_1.png',
+        '../static/homepage content/lights_pic_2.png',
+        '../static/homepage content/lights_pic_3.png',
+    ];
+
+    var currentIndex = 0;
+
+    function showCurrentSlide() {
+        var slideshowContainer = document.querySelector('.slideshow-container');
+        slideshowContainer.innerHTML = '<div class="mySlides">' +
+            '<img src="' + imageUrls[currentIndex] + '" alt="Slide ' + (currentIndex + 1) + '">' +
+            '</div>';
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        showCurrentSlide();
+    }
+
+    showCurrentSlide();
+
+    setInterval(nextSlide, 5000);
+
 });
 
-var slideIndex = 1;
-
-function showSlides(n) {
-    var slides = $('.mySlides');
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    slides.hide();
-    $(slides[slideIndex - 1]).show();
-}
-
-setInterval(function () {
-    slideIndex++;
-    showSlides(slideIndex);
-}, 5000); // Change slide every 5 seconds
