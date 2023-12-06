8568deecd16eabdcdd7c7ded571d3f9474f1dce8
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-12-06 23:21:40 +0000

    Sike using 3 table again
    Forgot about storing the finished lights too, so I added another table to cover that. Winners are naturally filtered by using the referenced IDs present in this table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,6 @@
 # DROP TABLE IF EXISTS Submissions;
 # DROP TABLE IF EXISTS Ratings;
-
-
-CREATE TABLE IF NOT EXISTS Submissions (
+CREATE TABLE IF NOT EXISTS Drawings (
 
     id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     filename VARCHAR(128) NOT NULL,
@@ -10,8 +8,15 @@ CREATE TABLE IF NOT EXISTS Submissions (
     mime_type ENUM('image/jpeg', 'image/png') NOT NULL,
     submission_year YEAR NOT NULL,
     year_group VARCHAR(50) NOT NULL,
-    `name` VARCHAR(128),
-    winner BOOLEAN
+    `name` VARCHAR(128)
+
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Lights (
+
+    id BIGINT PRIMARY KEY NOT NULL,
+    filename VARCHAR(128) NOT NULL,
+    mime_type ENUM('image/jpeg', 'image/png') NOT NULL
 
 ) ENGINE=InnoDB;
 
@@ -22,6 +27,6 @@ CREATE TABLE IF NOT EXISTS Ratings (
    comment VARCHAR(128) NULL, -- Should be allowed to be NULL because the user may just want to submit a like.
    liked BOOLEAN, -- The like button will act as a check box, so null values cannot be entered, even though they're allowed.
 
-   FOREIGN KEY (id) REFERENCES Submissions(id)
+   FOREIGN KEY (id) REFERENCES Drawings(id)
 
 ) ENGINE=InnoDB;
