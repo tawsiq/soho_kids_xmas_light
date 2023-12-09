7a97f1aca2abf0e4b3e15bacc524d6a1327e967b
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-12-09 13:52:29 +0000

    Adding everything that was deleted

---
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/submissions/ImageRepository.java
@@ -0,0 +1,12 @@
+package uk.ac.cf.client1.team7sohokidschristmaslights.submissions;
+
+import java.util.List;
+
+// Working with the database to retrieve submission information.
+public interface ImageRepository {
+    List<List<ImageClass>> getImageItemList();
+    ImageClass getImage(Long id, Boolean light); // use the primary key to locate specific images in the database. Whether it's a light or a drawing is dependent on which table is used.
+    List<RatingClass> getRatingList(Long submission_id);
+    Boolean lightCounterpartPresent(Long id);
+    void addRatingToSubmission(RatingClass rating);
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS Ratings;
+DROP TABLE IF EXISTS Lights;
+DROP TABLE IF EXISTS Drawings;
+
+CREATE TABLE IF NOT EXISTS Drawings (
+
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    filename VARCHAR(128) NOT NULL,
+    filepath VARCHAR(255) NOT NULL,
+    mime_type ENUM('image/jpeg', 'image/png', 'image/jpg') NOT NULL,
+    submission_year YEAR NOT NULL,
+    year_group VARCHAR(50) NOT NULL,
+    `name` VARCHAR(128)
+
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Lights (
+
+    drawing_id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    filename VARCHAR(128) NOT NULL,
+    filepath VARCHAR(255) NOT NULL,
+    mime_type ENUM('image/jpeg', 'image/png', 'image/jpg') NOT NULL,
+
+    FOREIGN KEY (drawing_id) REFERENCES Drawings(id)
+
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Ratings (
+
+    rating_id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    submission_id BIGINT NOT NULL,
+    `name` VARCHAR(128) NOT NULL,
+    `comment` VARCHAR(128) NULL, -- Should be allowed to be NULL because the user may just want to submit a like.
+    liked BOOLEAN, -- The like button will act as a check box, so null values cannot be entered, even though they're allowed.
+    `date_time` VARCHAR(64) NOT NULL,
+
+    FOREIGN KEY (submission_id) REFERENCES Drawings(id)
+
+) ENGINE=InnoDB;
