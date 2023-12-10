beb262e0f01cb10a986091bd8a66d66e688d554d
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-12-10 22:09:35 +0000

    Filter no longer working but I've made a lot of progress on correcting the layout

--- src/main/resources/static/js/submissions-page/image-filter.js
+++ src/main/resources/static/js/submissions-page/image-filter.js
@@ -17,12 +17,34 @@ document.addEventListener('DOMContentLoaded', function () {
     function filterImages(criteria) {
         const images = document.querySelectorAll('.winner-submissions-flex-container .button.full-border, .submissions-flex-container .button.full-border');
 
+        // This tags element before their classes affecting styling are removed in order to keep track of them. Ones that satisfy the filter will have their classes reinstated
+        // logic is wrong here. I'll need to tag images in the html. 
         images.forEach(image => {
-            const imageYearGroup = image.dataset.yeargroup;
-            const imageSubmissionYear = image.dataset.submissionyear;
+                if (image.classList.contains('winner-submissions-flex-container')) {
+                    image.classList.add('winnerTag');
+                    image.classList.remove('winner-submissions-flex-container');
+                } else {
+                    // I have (hopefully) never assigned two elements with both classes queried
+                    // image.classList.add('submissionTag'); // technically don't need this
+                    image.classList.remove('submissions-flex-container');
+                }
+            }
+        )
+
+        images.forEach(image => {
+            const imageYearGroup = image.dataset.year_group;
+            const imageSubmissionYear = image.dataset.submission_year;
+
 
             if ((criteria === imageYearGroup) || (criteria === imageSubmissionYear)) {
                 image.classList.remove('hidden');
+
+                if (image.classList.contains('winnerTag')) {
+                    image.classList.add('winner-submissions-flex-container');
+                } else {
+                    image.classList.add('submissions-flex-container');
+                }
+
             } else {
                 image.classList.add('hidden');
                 console.log(`Filtered out: ${imageYearGroup} - ${imageSubmissionYear}`);
