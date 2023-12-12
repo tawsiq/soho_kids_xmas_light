4356c45b55321fbdd28acfd47fcd20b1e4c100e2
Mohammed Tuhin Rashid <rashidt1@cardiff.ac.uk>
2023-12-12 11:55:24 +0000

    added modelandview getmapping method to sponsor controller

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/aboutus/SponsorController.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/aboutus/SponsorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -39,5 +40,10 @@ public class SponsorController {
         return "redirect:/";
     }
 
-    @GetMapping
+    @GetMapping("home/aboutus")
+    public ModelAndView hostaboutus() {
+        ModelAndView modelAndView = new ModelAndView("about-us-page/aboutus2");
+
+        return modelAndView;
+    }
 }
