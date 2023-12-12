720d6dc3fe845539ef0e7e6bbc0884f6bbf0e677
Mohammed Tuhin Rashid <rashidt1@cardiff.ac.uk>
2023-12-12 09:58:21 +0000

    beginning to add js to media links

--- src/main/resources/templates/about-us-page/aboutus2.js
+++ src/main/resources/templates/about-us-page/aboutus2.js
@@ -9,3 +9,14 @@ function validateEmail() {
 
     return true;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var mediaLinks = document.querySelectorAll('.media-links a');
+
+    mediaLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            link.style.color = 'red';
+        });
+    });
+});
