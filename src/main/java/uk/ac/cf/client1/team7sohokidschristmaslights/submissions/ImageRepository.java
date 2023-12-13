51c276cdc5c72577d1a6a045fc748c8d2c11c6e1
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-12-13 20:46:39 +0000

    Tested changes, all seems clear. Requesting merge to main now.

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/submissions/ImageRepository.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/submissions/ImageRepository.java
@@ -2,7 +2,6 @@ package uk.ac.cf.client1.team7sohokidschristmaslights.submissions;
 
 import java.util.List;
 
-// Working with the database to retrieve submission information.
 public interface ImageRepository {
     List<List<ImageClass>> getImageItemList();
     ImageClass getImage(Long id, Boolean light); // use the primary key to locate specific images in the database. Whether it's a light or a drawing is dependent on which table is used.
