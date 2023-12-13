cd719629e3834788a7f914779222c12a04e77534
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-12-13 17:12:21 +0000

    Routed JS function to controller
    Like button works as expected, but only when the post review button is pressed. This should happen then the like button is pressed.

--- src/main/resources/static/js/submissions-page/async-ratings.js
+++ src/main/resources/static/js/submissions-page/async-ratings.js
@@ -3,13 +3,13 @@
 function sendRating(){
     const raterName = document.forms["submitRating"]["raterName"].value;
     const commentText = document.forms["submitRating"]["commentText"].value;
-    const liked = document.forms["submitRating"]["liked"].checked;
+    // const liked = document.forms["submitRating"]["liked"].checked; // Commented this out because rating no longer contains this field. Also removed from params below
 
     // This'll make up the POST message content. Notice the form is a key-value pair.
-    let params = `raterName=${raterName}&commentText=${commentText}&liked=${liked}`;
+    let params = `raterName=${raterName}&commentText=${commentText}`;
     let dynamicFormAction = document.getElementById("submitRating").getAttribute("data-action");
     // Initiate a blank message, open its route, and label it with a header. Form's action goes here.
-    const xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
     xhttp.open("POST", dynamicFormAction, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     // Upon a message state change, execute the following function:
@@ -60,7 +60,48 @@ function fetchLikes() {
             console.error('Error fetching likes:', error);
         });
 }
+// Obtain the checkbox so that we can process requests on state change
 
+function sendLikeUpdate(increment){
+    // Mirrors the above post request method, except this doesn't take in the field's data directly. Increment is decided by the state change of the like button.
+    // this will be '/home/submissions/' + ${drawing.getId()} + '/updateLikeCount/', so we need to add the increment passed in by the checker below.
+    let dynamicFormAction_WithoutIncrement = document.getElementById("liked").getAttribute("data-sendLikeEndpoint_withoutIncrement");
+    let dynamicFormAction = dynamicFormAction_WithoutIncrement + increment;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", dynamicFormAction, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // Upon a message state change, execute the following function:
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            // Response is logged in the console
+            console.log(xhttp.responseText);
+            fetchLikes(); // to update the counter when like is pressed. No need to fetch comments here.
+        } else {
+            // If response failed in any way, log the error into the console.
+            console.error(xhttp.statusText);
+        }
+    };
+    // Send the value (decided by the event listener below) to increment the like count by to the controller.
+    xhttp.send(increment);
+    return false;
+
+}
+const checkbox = document.getElementById("liked");
+checkbox.addEventListener('change', function (event) {
+
+    if (event.target.checked) {
+        // This block will run when the checkbox is checked
+        sendLikeUpdate(1);
+        console.log('Checkbox is checked now');
+        // Add your code here
+    } else {
+        // and this will run when the checkbox is unchecked
+        sendLikeUpdate(-1);
+        console.log('Checkbox is unchecked now');
+        // Add your code here
+    }
+});
 // So that comments don't need to be directly passed by the controller the first time the URL is accessed.
 // Filtered rating code & fetching like counter in GetMapping controller no longer needs to be repeated (see where it was commented out)
 document.addEventListener('DOMContentLoaded', fetchComments);
