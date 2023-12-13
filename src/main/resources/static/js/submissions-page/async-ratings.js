c0ffc1f6cc6a71624bafb9abfbed5ca7107c4c13
Moustafa El-Shaboury <moustafaelshaboury2@gmail.com>
2023-12-13 20:27:45 +0000

    Documented Async JS

--- src/main/resources/static/js/submissions-page/async-ratings.js
+++ src/main/resources/static/js/submissions-page/async-ratings.js
@@ -5,8 +5,9 @@ function sendRating(){
     const commentText = document.forms["submitRating"]["commentText"].value;
     // const liked = document.forms["submitRating"]["liked"].checked; // Commented this out because rating no longer contains this field. Also removed from params below
 
-    // This'll make up the POST message content. Notice the form is a key-value pair.
+    // This'll make up the POST message content for the form's input name & comment text.
     let params = `raterName=${raterName}&commentText=${commentText}`;
+    // Form action retrieved via data attribute so that th: can still be used. To dynamically render the id. Alternatively I could have just extracted the url.
     let dynamicFormAction = document.getElementById("submitRating").getAttribute("data-action");
     // Initiate a blank message, open its route, and label it with a header. Form's action goes here.
     let xhttp = new XMLHttpRequest();
@@ -18,7 +19,7 @@ function sendRating(){
             // Response is logged in the console
             console.log(xhttp.responseText);
             fetchComments();
-            // fetchLikes(); // on form submission to update its number too.
+            // And only fetch comments. Likes will now be fetched separately.
         } else {
             // If response failed in any way, log the error into the console.
             console.error(xhttp.statusText);
@@ -30,14 +31,15 @@ function sendRating(){
     return false;
 }
 function fetchComments() {
-    // Asynchronous request to get updated comments
+    // Asynchronous request to get updated comments. The fetch method was a lot easier to implement that the GET / POST method above for this.
+    // The controllers had to be changed to get this to work, but it should still be readable.
     const commentsContainer = document.querySelector('.comment-section');
-    //Obtaining endpoint defined by Thymeleaf in the container:
+    // Obtaining endpoint defined by Thymeleaf in the container. See HTML & Controller endpoint pair.:
     const commentsEndpoint = commentsContainer.getAttribute("data-commentsEndpoint")
     fetch(commentsEndpoint)
         .then(response => response.text())
         .then(data => {
-            // Replaces the existing comments section with the updated comments HTML, defined in the controller with this endpoint.
+            // Replaces the existing comments section with the updated comments HTML defined in the controller with this endpoint.
             commentsContainer.innerHTML = data;
         })
         .catch(error => {
@@ -45,9 +47,9 @@ function fetchComments() {
         });
 }
 function fetchLikes() {
-    // Asynchronous request to get updated comments
+    // Asynchronous request to get updated likes. Mirrors the one above for comments. This is kept separate for clarity and ease of endpoint routing. Not very dry tho.
     const likesContainer = document.getElementById("likeCount")
-    //Obtaining endpoint defined by Thymeleaf in the container:
+    // Again obtaining endpoint defined by Thymeleaf in the container:
     const likesEndpoint = likesContainer.getAttribute("data-likesEndpoint")
 
     fetch(likesEndpoint)
@@ -60,15 +62,17 @@ function fetchLikes() {
             console.error('Error fetching likes:', error);
         });
 }
-
+// This one is called within the html itself, where the onchange attribute is used with 1 or -1.
 function sendLikeUpdate(increment) {
-
+    // Again very similar, except this one's a fetch post request. I tried to follow the blueprint of lectures like I did for sendRating,
+    //  but it didn't end up working since we aren't specifically sending in a form tied to an object.
     let sendLikeEP_WithoutIncrement = document.getElementById("liked").getAttribute("data-sendLikeEndpoint_withoutIncrement");
     let sendLikeEndPoint = sendLikeEP_WithoutIncrement + increment;
 
     fetch(sendLikeEndPoint, {
         method: 'POST',
-        // Add any necessary headers or body if needed
+        // Didn't really need content headers here, although not including them is probably bad practise.
+        // I included them on the sendRating function because that's how the lectures did it, but I didn't end up using those either.
     })
         .then(response => {
             if (response.ok) {
