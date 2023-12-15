60e43169e367c0ccbf226f2b2dbe862658059887
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-15 18:32:15 +0000

    updated sql with product images table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -49,3 +49,16 @@ CREATE TABLE IF NOT EXISTS LikeCounts (
    -- FOREIGN KEY (submission_id) REFERENCES Drawings(id)
 
 ) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS MarketplaceProducts (
+
+   product_id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+   filename VARCHAR(128) NOT NULL,
+   filepath VARCHAR(255) NOT NULL
+
+-- Every time a like is submitted, the entry corresponding to the submission id will be updated.
+-- No duplicates can be entered due to the way implementation will work.
+
+-- FOREIGN KEY (submission_id) REFERENCES Drawings(id)
+
+) ENGINE=InnoDB;
