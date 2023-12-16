9b86a45d28ca44bd9422506a6ac5c5cdeff25de1
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-16 11:56:30 +0000

    Updated data.sql with product image paths and details

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,16 @@
 DELETE FROM MarketplaceProducts;
 
-INSERT INTO menu_item (name, price_in_pence, allergens, description, is_vegetarian, is_vegan)
-VALUES ('Egg and Bacon', 100, 'egg', '', FALSE, FALSE);
+INSERT INTO MarketplaceProducts (product_id, filename, filepath)
+VALUES (1, 'XMAS T-Shirt 001', 'C:\Projects\Soho_kids_project_softeng\team-7-soho-kids-christmas-lights\src\main\resources\static\images\marketplace-productlist\Products\Black-Tshirt-Arm-Logo.png');
 
-INSERT INTO menu_item (name, price_in_pence, allergens, description, is_vegetarian, is_vegan)
-VALUES ('Egg Sausage and Bacon', 200, 'egg', '', FALSE, FALSE);
+INSERT INTO MarketplaceProducts (product_id, filename, filepath)
+VALUES (2, 'XMAS T-Shirt 002', 'C:\Projects\Soho_kids_project_softeng\team-7-soho-kids-christmas-lights\src\main\resources\static\images\marketplace-productlist\Products\Black-T-Shirt.png');
 
-INSERT INTO menu_item (name, price_in_pence, allergens, description, is_vegetarian, is_vegan)
-VALUES ('Egg and Spam', 200, 'egg', '', FALSE, FALSE);
+INSERT INTO MarketplaceProducts (product_id, filename, filepath)
+VALUES (3, 'XMAS T-Shirt 003', 'C:\Projects\Soho_kids_project_softeng\team-7-soho-kids-christmas-lights\src\main\resources\static\images\marketplace-productlist\Products\White-T-Shirt.png');
 
-INSERT INTO menu_item (name, price_in_pence, allergens, description, is_vegetarian, is_vegan)
-VALUES ('Egg, Bacon and Spam', 250, 'egg', '', FALSE, FALSE);
+INSERT INTO MarketplaceProducts (product_id, filename, filepath)
+VALUES (4, 'Gingerbread Card', 'C:\Projects\Soho_kids_project_softeng\team-7-soho-kids-christmas-lights\src\main\resources\static\images\marketplace-productlist\Products\Gingerbread-Greeting-Card.jpg');
+
+INSERT INTO MarketplaceProducts (product_id, filename, filepath)
+VALUES (5, 'Wrapping Paper 2022', 'C:\Projects\Soho_kids_project_softeng\team-7-soho-kids-christmas-lights\src\main\resources\static\images\marketplace-productlist\Products\Wrapping-Paper.jpgg');
