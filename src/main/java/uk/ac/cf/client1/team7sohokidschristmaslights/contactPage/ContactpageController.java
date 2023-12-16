57a311a89016d9156f4126dc5f7a76cc78e3e160
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 21:06:07 +0000

    Adding comments, creating Contact Page Controller, will create 'ContactServiceImp' and other variables next

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactpageController.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactpageController.java
@@ -5,4 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class ContactpageController {
 
+    // Declare a reference to the ContactService
+    ContactService contactService;
+
+    // Constructor to inject the ContactService dependency
+    public ContactpageController(ContactService contactService){
+        this.contactService = contactService;
+    }
+
+    // Handler method for the "/home/contactus" URL
+    @GetMapping("/home/contactus")
+    public ModelAndView hostContactPage(){
+        // Return a new ModelAndView with the view name "NewContactUs/NewContactUs"
+        return new ModelAndView("NewContactUs/NewContactUs");
+    }
+
+    // Handler method for the "/home/contactus/contact-details" URL
+    @GetMapping("/home/contactus/contact-details")
+    public ModelAndView hostContactDetailsPage(){
+        // Create a new ModelAndView with the view name "NewContactUs/new-contact-details"
+        ModelAndView modelAndView = new ModelAndView("NewContactUs/new-contact-details");
+        // Add the "contactsList" attribute to the model, populated from the ContactService
+        modelAndView.addObject("contactsList", contactService.getContactList());
+        // Return the populated ModelAndView
+        return modelAndView;
+    }
+}
