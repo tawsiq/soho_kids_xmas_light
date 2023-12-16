480d0d51187847bfc5bb7164acc7c7f4dcfacfc6
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-16 15:14:58 +0000

    Added Lombok annotations. Addition of @Data and @AllArgsConstructor annotations. Defined fields in Product entity corresponding to database columns. Added definition of product_id, filename, and filepath fields.

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/marketplace/Product.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/marketplace/Product.java
@@ -1,19 +1,14 @@
 package uk.ac.cf.client1.team7sohokidschristmaslights.marketplace;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long productId;
 
+    private Long product_id;
     private String filename;
     private String filepath;
-
-    // Getters and setters
-    // ...
 }
