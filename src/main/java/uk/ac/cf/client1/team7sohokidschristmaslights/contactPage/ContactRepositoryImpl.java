05ba2ff72353479aa882c1a80fd81bd9fc3a457f
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 21:29:35 +0000

    Adding rs.getString to map the name column to the 'contact' name field

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepositoryImpl.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepositoryImpl.java
@@ -28,8 +28,8 @@ public class ContactRepositoryImpl implements ContactRepository {
         // Define the contactRowMapper using a lambda expression
         contactRowMapper = (rs, i) -> new Contact(
                 rs.getInt("id"),          // Map the "id" column from the result set to the Contact's id field
+                rs.getString("name"),     // Map the "name" column to the Contact's name field
 
-                
         );
     }
 }
