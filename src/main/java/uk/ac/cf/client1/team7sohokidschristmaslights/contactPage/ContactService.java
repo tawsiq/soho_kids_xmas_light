0ce479ab4d8faab962ecdf3016f4ee3da828f75b
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 21:45:57 +0000

    Adding packages, importing java list, creating public interface ContactService, adding ContactLists, and adding comments aswell

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactService.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactService.java
@@ -1,4 +1,13 @@
 package uk.ac.cf.client1.team7sohokidschristmaslights.contactPage;
 
+import java.util.List;
+
+// Declare an interface for managing contact-related services
 public interface ContactService {
+
+    // Method to retrieve a list of contacts
+    List<Contact> getContactList();
+
+    // Method to retrieve a specific contact by ID
+    Contact getContact(Integer id);
 }
