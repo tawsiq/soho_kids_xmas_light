4218b9f47efaf312a682537715bec717e8d7aaad
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-16 19:01:27 +0000

    fixed images issue

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 DELETE FROM MarketplaceProducts;
 
 INSERT INTO MarketplaceProducts (product_id, filename, filepath, product_name, price)
-VALUES (1, 'Black-Tshirt-Arm-Logo.png', '/images/marketplace-productlist/Products/Black-Tshirt-Arm-Logo.png', 'Black T-shirt with Arm Logo', 30.00);
+VALUES (1, 'Black-Tshirt-Arm-Logo.png', 'C:\\Projects\\Soho_kids_project_softeng\\team-7-soho-kids-christmas-lights\\src\\main\\resources\\static\\images\\marketplace-productlist\\Products\\Black-Tshirt-Arm-Logo.png', 'Black T-shirt with Arm Logo', 30.00);
 
 INSERT INTO MarketplaceProducts (product_id, filename, filepath, product_name, price)
-VALUES (2, 'Black-T-Shirt.png', '/images/marketplace-productlist/Products/Black-T-Shirt.png', 'Black T-Shirt', 28.00);
+VALUES (2, 'Black-T-Shirt.png', 'C:\\Projects\\Soho_kids_project_softeng\\team-7-soho-kids-christmas-lights\\src\\main\\resources\\static\\images\\marketplace-productlist\\Products\\Black-T-Shirt.png', 'Black T-Shirt', 28.00);
 
 INSERT INTO MarketplaceProducts (product_id, filename, filepath, product_name, price)
-VALUES (3, 'White-T-Shirt.png', '/images/marketplace-productlist/Products/White-T-Shirt.png', 'White T-Shirt', 28.00);
+VALUES (3, 'White-T-Shirt.png', 'C:\\Projects\\Soho_kids_project_softeng\\team-7-soho-kids-christmas-lights\\src\\main\\resources\\static\\images\\marketplace-productlist\\Products\\White-T-Shirt.png', 'White T-Shirt', 28.00);
 
 INSERT INTO MarketplaceProducts (product_id, filename, filepath, product_name, price)
-VALUES (4, 'Gingerbread-Greeting-Card.jpg', '/images/marketplace-productlist/Products/Gingerbread-Greeting-Card.jpg', 'Gingerbread Greeting Card', 5.00);
+VALUES (4, 'Gingerbread-Greeting-Card.jpg', 'C:\\Projects\\Soho_kids_project_softeng\\team-7-soho-kids-christmas-lights\\src\\main\\resources\\static\\images\\marketplace-productlist\\Products\\Gingerbread-Greeting-Card.jpg', 'Gingerbread Greeting Card', 5.00);
 
 INSERT INTO MarketplaceProducts (product_id, filename, filepath, product_name, price)
-VALUES (5, 'Wrapping-Paper.jpg', '/images/marketplace-productlist/Products/Wrapping-Paper.jpg', 'Wrapping Paper 2022', 10.00);
+VALUES (5, 'Wrapping-Paper.jpg', 'C:\\Projects\\Soho_kids_project_softeng\\team-7-soho-kids-christmas-lights\\src\\main\\resources\\static\\images\\marketplace-productlist\\Products\\Wrapping-Paper.jpg', 'Wrapping Paper 2022', 10.00);
 --Delete product ID
