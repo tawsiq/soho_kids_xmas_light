cf84bf0c7be681040d26a784a02dfcd01f85ab81
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 21:28:44 +0000

    Adding all private fields, templates, RowMappers, classes, etc

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepositoryImpl.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepositoryImpl.java
@@ -1,4 +1,35 @@
 package uk.ac.cf.client1.team7sohokidschristmaslights.contactPage;
 
-public class ContactRepositoryImpl {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// Mark the class as a repository (data access component) using the @Repository annotation
+@Repository
+public class ContactRepositoryImpl implements ContactRepository {
+
+    // Declare a private field to hold a reference to JdbcTemplate
+    private final JdbcTemplate jdbcTemplate;
+
+    // Declare a field for the RowMapper responsible for mapping database rows to Contact objects
+    private RowMapper<Contact> contactRowMapper;
+
+    // Constructor: Inject the JdbcTemplate dependency
+    public ContactRepositoryImpl(JdbcTemplate aJdbc) {
+        this.jdbcTemplate = aJdbc;
+        // Initialize the contactRowMapper
+        setContactRowMapper();
+    }
+
+    // Private method to set up the contactRowMapper
+    private void setContactRowMapper() {
+        // Define the contactRowMapper using a lambda expression
+        contactRowMapper = (rs, i) -> new Contact(
+                rs.getInt("id"),          // Map the "id" column from the result set to the Contact's id field
+
+                
+        );
+    }
 }
