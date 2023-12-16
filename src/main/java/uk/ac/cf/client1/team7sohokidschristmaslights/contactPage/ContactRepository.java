fc293c4efa712cea261e5744e1b4b84e339f5e29
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 21:21:11 +0000

    Adding code along with comments for Contact Repository

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepository.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepository.java
@@ -1,3 +1,13 @@
 package uk.ac.cf.client1.team7sohokidschristmaslights.contactPage;
 
 import java.util.List;
+
+// Declare an interface for managing contacts
+public interface ContactRepository {
+
+    // Method to retrieve a list of contacts
+    List<Contact> getContactList();
+
+    // Method to retrieve a specific contact by ID
+    Contact getContact(Integer id);
+}
