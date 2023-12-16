1572c5dfcc05732a7421382f5a9d257b66d6e8e6
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 22:00:07 +0000

    Adding final code parts and comments

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactServiceImpl.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactServiceImpl.java
@@ -4,5 +4,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class ContactServiceImpl {
+// Mark the class as a service component using the @Service annotation
+@Service
+public class ContactServiceImpl implements ContactService {
+
+    // Declare a reference to the ContactRepository
+    ContactRepository contactRepository;
+
+    // Constructor: Inject the ContactRepository dependency
+    public ContactServiceImpl(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    // Method to retrieve a specific contact by ID
+    public Contact getContact(Integer id) {
+        return contactRepository.getContact(id);
+    }
+
+    // Method to retrieve a list of contacts
+    public List<Contact> getContactList() {
+        return contactRepository.getContactList();
+    }
 }
