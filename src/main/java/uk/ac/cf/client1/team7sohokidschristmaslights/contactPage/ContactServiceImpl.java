46431d5bcb0e066df611c62d189e734d96969369
Ozair <shadmo@cardiff.ac.uk>
2023-12-16 21:47:59 +0000

    Importing relevant frameworks and java lists

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactServiceImpl.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactServiceImpl.java
@@ -1,4 +1,8 @@
 package uk.ac.cf.client1.team7sohokidschristmaslights.contactPage;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 public class ContactServiceImpl {
 }
