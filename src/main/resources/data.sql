f55f3899d43036b15ed6485cca9a58cab7e863bc
Ozair <shadmo@cardiff.ac.uk>
2023-12-17 10:48:43 +0000

    Merging into main

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-DELETE FROM ContactInfo;
-INSERT INTO ContactInfo (`name`, email, `subject`, message) VALUES ('Ozair', 'ozair@gmail.com', 'I like milk', 'Just wanted to get in contact with you to say that I really like milk... and speed typing.')
+/*Have commented this out as was testing SQL before so using this as dummy test, but is no longer needed*/
+
+/*DELETE FROM ContactInfo;
+INSERT INTO ContactInfo (`name`, email, `subject`, message) VALUES ('Ozair', 'ozair@gmail.com', 'I like milk', 'Just wanted to get in contact with you to say that I really like milk... and speed typing.')*/
