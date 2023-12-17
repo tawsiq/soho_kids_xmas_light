909aade23500e3b52fbfe4d1bbc4731a21a8742f
Ozair <shadmo@cardiff.ac.uk>
2023-12-17 13:10:16 +0000

    Trying to fix clashes with JavaScript files and code

--- src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepository.java
+++ src/main/java/uk/ac/cf/client1/team7sohokidschristmaslights/contactPage/ContactRepository.java
@@ -7,9 +7,18 @@ public interface ContactRepository {
 
     // Method to retrieve a list of contacts
     List<Contact> getContactList();
+    // This method returns a list of contacts. Implementations of this interface
+    // will provide the actual logic to fetch the contact data.
 
     // Method to retrieve a specific contact by ID
     Contact getContact(Integer id);
+    // This method retrieves a specific contact based on the provided 'id'.
+    // Implementations of this interface will handle the retrieval of a contact
+    // with the given ID.
 
     void saveContactDetails(Contact contact);
+    // This method saves the details of a contact. The 'contact' parameter contains
+    // the information to be saved (e.g., name, email, phone number).
+    // Implementations of this interface will handle the actual saving process,
+    // such as storing the data in a database or other storage mechanism.
 }
