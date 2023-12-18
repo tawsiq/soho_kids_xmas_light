ff490a944b2306b65096089b95f688a5f948b51c
Mohammed Tuhin Rashid <rashidt1@cardiff.ac.uk>
2023-12-18 10:11:34 +0000

    commented out data.sql code as dummy info no longer needed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-DELETE FROM SponsorInfo;
-INSERT INTO SponsorInfo (company_name, contact_person, email)
-VALUES ('RashidInc','Tuhin','Tuhin@gmail.com');
+#DELETE FROM SponsorInfo;
+#INSERT INTO SponsorInfo (company_name, contact_person, email)
+#VALUES ('RashidInc','Tuhin','Tuhin@gmail.com');
