deb0c0ef314009bb6e1e233749e2d6d51d5a37e1
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2023-12-18 12:25:28 +0000

    adding a order summary

--- src/main/resources/static/js/checkout-page/checkout.js
+++ src/main/resources/static/js/checkout-page/checkout.js
@@ -14,39 +14,24 @@ function cartIsEmpty() {
     // Implement a check to see if the cart is empty
     return cart.length === 0;
 }
-// Assuming 'cart' is your cart array and each item has 'price' and 'quantity' properties
 
-// Function to calculate total price
-function calculateTotalPrice() {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-}
-
-// Function to update total price in the hidden input field
-function updateTotalPriceInCheckout() {
-    const totalPrice = calculateTotalPrice();
-    document.getElementById('totalPriceInput').value = totalPrice.toFixed(2);
-}
-
-// Bind this function to the form's submit event or any event that changes the cart
-const checkoutForm = document.querySelector('form');
-checkoutForm.addEventListener('submit', updateTotalPriceInCheckout);
-
-function displayOrderSummary() {
-    let summaryHtml = '';
-    cart.forEach(item => {
-        summaryHtml += `<div>${item.name} - Quantity: ${item.quantity} - Price: £${item.price.toFixed(2)}</div>`;
-    });
-    summaryHtml += `<div>Total Cost: £${calculateTotalCost().toFixed(2)}</div>`;
-    document.getElementById('orderSummary').innerHTML = summaryHtml;
-}
-
-function calculateTotalCost() {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-}
-
-// Call this function when the page loads
-displayOrderSummary();
-document.addEventListener('DOMContentLoaded', function() {
-    displayOrderSummary();
+document.addEventListener('DOMContentLoaded', () => {
+    OrderSummary();
 });
 
+function OrderSummary() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let orderSummaryContainer = document.getElementById('order-summary');
+
+    cart.forEach(item => {
+        let itemElement = document.createElement('div');
+        itemElement.classList.add('order-item');
+        itemElement.innerHTML = `
+            <h3>${productName}</h3>
+            <p>Price: £${item.price}</p>
+            <p>Quantity: ${item.quantity}</p>
+            <p>Total: £${(item.price * item.quantity).toFixed(2)}</p>
+        `;
+        orderSummaryContainer.appendChild(itemElement);
+    });
+}
