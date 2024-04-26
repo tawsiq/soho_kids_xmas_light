5344e708550f5f3f4aaf1280a8b827531f7ca476
Mahmud Bhuiyan <bhuiyanmt@cardiff.ac.uk>
2024-04-26 16:25:26 +0000

    Upload New File

---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "flavor" { default = "m1.large" }
+variable "image" { default = "Debian Buster 10.11.1 20211029" }
+variable "name1" { default = "DebbySoho" }
+
+variable "keypair" { default = "osproject" } # you may need to change this
+variable "network" { default = "openStack_project_network" }   # you need to change this
+
+variable "pool" { default = "cscloud_private_floating" }
+variable "server1_script" { default = "./server1.sh" }
+variable "security_description" { default = "Terraform security group" }
+variable "security_name" { default = "tf_securityMat" }
